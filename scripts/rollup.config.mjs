064b3578a66ad05a191232145a0d22c59313906e
lushonline <56685070+lushonline@users.noreply.github.com>
2023-06-29 08:53:12 +0100

    Update build script

--- scripts/rollup.config.mjs
+++ scripts/rollup.config.mjs
@@ -10,102 +10,139 @@ import { fileURLToPath } from 'url';
 import license from 'rollup-plugin-license';
 import bundleSize from 'rollup-plugin-bundle-size';
 import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const version = process.env.npm_package_version;
 
-export default {
-  input: './src/index.mjs',
-  plugins: [
-    replace({
-      preventAssignment: true,
-      values: {
-        ___version___: version,
-      },
-    }),
-    resolve({
-      browser: true,
-      main: true,
-      jsnext: true,
-    }),
-    json(),
-    commonjs({
-      sourceMap: false,
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      babelrc: false,
-      exclude: 'node_modules/**',
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            loose: true,
-            modules: false,
-            spec: true,
-            forceAllTransforms: true,
-            useBuiltIns: 'usage',
-            corejs: 3,
+export default [
+  // Browser Builds
+  {
+    input: './src/index.mjs',
+    plugins: [
+      replace({
+        preventAssignment: true,
+        values: {
+          ___version___: version,
+        },
+      }),
+      resolve(),
+      json(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+      }),
+      license({
+        banner: {
+          content: {
+            file: join(__dirname, 'banner.ejs'),
+            encoding: 'utf-8', // Default is utf-8
           },
-        ],
-      ],
-      plugins: ['@babel/transform-object-assign'],
-    }),
-    license({
-      banner: {
-        content: {
-          file: join(__dirname, 'banner.ejs'),
-          encoding: 'utf-8', // Default is utf-8
+          data: {
+            version,
+          },
         },
-        data: {
-          version,
-        },
+      }),
+      bundleSize(),
+    ],
+    output: [
+      {
+        file: './dist/index.umd.js',
+        format: 'umd',
+        name: 'MyClass',
+        esModule: false,
+        exports: 'named',
+        sourcemap: true,
       },
-    }),
-    bundleSize(),
-  ],
-  output: [
-    {
-      file: './dist/index.umd.js',
-      format: 'umd',
-      name: 'YOUR_MODULE_NAME',
-      esModule: false,
-      exports: 'named',
-      sourcemap: true,
-    },
-    {
-      file: './dist/index.umd.min.js',
-      format: 'umd',
-      name: 'YOUR_MODULE_NAME',
-      esModule: false,
-      exports: 'named',
-      sourcemap: true,
-    },
-    {
-      file: './dist/index.js',
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-    },
-    {
-      file: './dist/index.min.js',
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-    },
-    {
-      file: './dist/index.es.mjs',
-      format: 'es',
-      exports: 'named',
-      sourcemap: true,
-    },
-    {
-      file: './dist/index.es.min.mjs',
-      format: 'es',
-      exports: 'named',
-      sourcemap: true,
-    },
-  ],
-};
+    ],
+  },
+  // Browser Builds - Minified
+  {
+    input: './src/index.mjs',
+    plugins: [
+      replace({
+        preventAssignment: true,
+        values: {
+          ___version___: version,
+        },
+      }),
+      resolve(),
+      json(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+      }),
+      terser(),
+      license({
+        banner: {
+          content: {
+            file: join(__dirname, 'banner.ejs'),
+            encoding: 'utf-8', // Default is utf-8
+          },
+          data: {
+            version,
+          },
+        },
+      }),
+      bundleSize(),
+    ],
+    output: [
+      {
+        file: './dist/index.umd.min.js',
+        format: 'umd',
+        name: 'MyClass',
+        esModule: false,
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+  },
+  // Node Builds
+  {
+    input: './src/index.mjs',
+    external: ['check-types'],
+    plugins: [
+      replace({
+        preventAssignment: true,
+        values: {
+          ___version___: version,
+        },
+      }),
+      json(),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+      }),
+      license({
+        banner: {
+          content: {
+            file: join(__dirname, 'banner.ejs'),
+            encoding: 'utf-8', // Default is utf-8
+          },
+          data: {
+            version,
+          },
+        },
+      }),
+      bundleSize(),
+    ],
+    output: [
+      {
+        file: './dist/index.cjs.js',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        file: './dist/index.esm.mjs',
+        format: 'es',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+  },
+];
